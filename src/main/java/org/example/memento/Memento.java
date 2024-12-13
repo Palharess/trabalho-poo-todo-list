e352e35a38c015c6238c17fde74d69114af488c8
Gabriel Palhares <123818597+Palharess@users.noreply.github.com>
2024-12-13 10:21:39 -0300

    Update Memento.java

--- src/main/java/org/example/memento/Memento.java
+++ src/main/java/org/example/memento/Memento.java
@@ -1,16 +1,25 @@
 package org.example.memento;
 
+import org.example.task.SimpleTask;
 import org.example.task.Task;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Memento {
     private final List<Task> state;
 
     public Memento(List<Task> state) {
-        // copia imutavel para o undo
-        this.state = state;
+        List<Task> copy = new ArrayList<>();
+        for (Task t : state) {
+            //foi criado uma task clonad para evitar trabalhar com referencias diretas
+            Task clonedTask = new SimpleTask(t.getTitle(), t.getDescription());
+            clonedTask.setDone(t.isDone());
+            copy.add(clonedTask);
+        }
+        this.state = copy; // após isso state se tornou uma lista independente funcionando o undo para edit
     }
+
 
     public List<Task> getState() {
         return state;
